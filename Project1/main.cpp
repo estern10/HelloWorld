33806ff383a0b2e53a5349b35967a4caca6364ee
estern10 <stern0204@fvtc.edu>
2014-01-14 12:12:30 -0600

    initial check'n

---
+++ Project1/main.cpp
@@ -0,0 +1,19 @@
+
+#include <conio.h>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World"  << endl;
+	_getch();
+	cout << "GoodBye" << endl;
+
+	//cout << "GoodBye";
+
+	_getch();
+
+	return 1;
+}
